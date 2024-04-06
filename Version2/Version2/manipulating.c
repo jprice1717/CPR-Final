7efe1e9f1fdcc250c5c7c53ec23da5e923eec872
Joshua Price <jprice17@myseneca.ca>
2024-04-06 17:03:01 -0400

    finished manipulating.c

--- Version2/Version2/manipulating.c
+++ Version2/Version2/manipulating.c
@@ -6,7 +6,7 @@
 //V1
 
 void manipulating(void) {
-	printf("*** Start of concatenating Strings Demo ***\n");
+	printf("*** Start of Concatenating Strings Demo ***\n");
 	char     string1[BUFFER_SIZE * 2];
 	char     string2[BUFFER_SIZE];
 	do {
@@ -22,5 +22,30 @@ void manipulating(void) {
 		}
 	} while (strcmp(string1, "q") != 0);
 	printf("*** End of Concatenating Strings Demo ***\n\n");
+
+
+// V2
+	printf("*** Start of Comparing Strings Demo ***\n");
+	char compare1[BUFFER_SIZE];	
+	char compare2[BUFFER_SIZE];
+	int result;
+	do {
+		printf("Type the 1st string to compare (q - to quit):\n");
+		fgets(compare1, BUFFER_SIZE, stdin);
+		compare1[strlen(compare1) - 1] = '\0';
+		if (strcmp(compare1, "q") != 0) {
+			printf("Type the 2nd string to compare:\n");
+			fgets(compare2, BUFFER_SIZE, stdin);
+			compare2[strlen(compare2) - 1] = '\0';
+			result = strcmp(compare1, compare2);
+			if (result < 0)
+				printf("\'%s\' string is less than \'%s\'\n", compare1, compare2);
+			else if (result == 0)
+				printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2);
+			else
+				printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2);
+		}
+	} while (strcmp(compare1, "q") != 0);
+	printf("*** End of Comparing Strings Demo ***\n\n");
 
 }
