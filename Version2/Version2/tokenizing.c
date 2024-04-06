47a7ecd9d1c5fa1acd7dd9b20bfaf8ec3513e5fa
Joshua Price <jprice17@myseneca.ca>
2024-04-06 16:40:58 -0400

    Done v2 tokenizing.c

--- Version2/Version2/tokenizing.c
+++ Version2/Version2/tokenizing.c
@@ -3,24 +3,24 @@
 #define BUFFER_SIZE 300
 #include "tokenizing.h"
 
-// V1
+// V2
 void tokenizing(void) {
-	printf("*** Start of Tokenizing Words Demo ***\n");
-	char    words[BUFFER_SIZE];
-	char* nextWord = NULL;
-	int     wordsCounter;
+	printf("*** Start of Tokenizing Phrases Demo ***\n");
+	char    phrases[BUFFER_SIZE];
+	char* nextPhrase = NULL;
+	int     phrasesCounter;
 	do {
-		printf("Type a few words seperated by space (q - to quit):\n");
-		fgets(words, BUFFER_SIZE, stdin);
-		words[strlen(words) - 1] = '\0';
-		if (strcmp(words, "q") != 0) {
-			nextWord = strtok(words, " ");
-			wordsCounter = 1;
-			while (nextWord) {
-				printf("Word #%d is \'%s\'\n", wordsCounter++, nextWord);
-				nextWord = strtok(NULL, " ");
+		printf("Type a few phrases seperated by comma (q - to quit):\n");
+		fgets(phrases, BUFFER_SIZE, stdin);
+		phrases[strlen(phrases) - 1] = '\0';
+		if ((strcmp(phrases, "q") != 0)) {
+			nextPhrase = strtok(phrases, ",");
+			phrasesCounter = 1;
+			while (nextPhrase) {
+				printf("Phrase #%d is \'%s\'\n", phraseCounter++, nextPhrase);
+				nextPhrase = strtok(NULL, ",");
 			}
 		}
-	} while (strcmp(words, "q") != 0);
-	printf("*** End of Tokenizing Demo ***\n\n");
+	} while (strcmp(phrases, "q") != 0);
+	printf("*** End of Tokenizing Phrases Demo ***\n\n");
 }
