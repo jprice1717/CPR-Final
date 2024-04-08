7dd13c66ad48c88333dd34ce7289be56aac5a568
Joshua Price <jprice17@myseneca.ca>
2024-04-08 16:22:36 -0400

    added v2 to v3 converting.c

--- Version3/Version3/converting.c
+++ Version3/Version3/converting.c
@@ -18,4 +18,19 @@ void converting(void) {
 		}
 	} while (strcmp(intString, "q") != 0);
 	printf("*** End of Converting Strings to int Demo ***\n\n");
+
+	// V2
+	printf("*** Start of Converting Strings to Double Demo ***\n");
+	char doubleString[BUFFER_SIZE];
+	double doubleNumber;
+	do {
+		printf("Type the double numeric string (q - to quit):\n");
+		fgets(doubleString, BUFFER_SIZE, stdin);
+		doubleString[strlen(doubleString) - 1] = '\0';
+		if ((strcmp(doubleString, "q") != 0)) {
+			doubleNumber = atof(doubleString);
+			printf("Converted number is %f\n", doubleNumber);
+		}
+	} while (strcmp(doubleString, "q") != 0);
+	printf("*** End of Converting Strings to double Demo ***\n\n");
 }
